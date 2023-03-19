3dbb452b1de10a80d4a6c4948abd0b59bdb1c63c
elgraphica <elferenczi@outlook.com>
2023-03-19 22:21:32 +0000

    components set up complete

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,25 @@
-import '@/styles/globals.css'
+import React from 'react';
+import Banner from '../components/Banner';
+import Header from '../components/Header';
+import Nav from '../components/Nav';
+import About from '../components/About';
+import Services from '../components/Services';
+import Work from '../components/Work';
+import Contact from '../components/Contact';
+import './index.css';
+
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <div className='bg-site bg-no-repeat bg-cover overflow-hidden'>
+      <Header />
+      <Banner />
+      <Nav />
+      <About />
+      <Services />
+      <Work />
+      <Contact />
+      <div className='h-[4000px]'></div>
+    </div>
+  );
 }
